d3f9e65b371f99dd4e6840ee783548f729ef17a6
zerodsLyn <zerodseu@gmail.com>
2020-04-18 23:11:47 +0800

    permute

--- src/main/java/com/zerods/learn/interview/algorithm/backtrack/Permute.java
+++ src/main/java/com/zerods/learn/interview/algorithm/backtrack/Permute.java
@@ -1,7 +1,9 @@
 package com.zerods.learn.interview.algorithm.backtrack;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * @author gengchao05 create on 2020/03/02
